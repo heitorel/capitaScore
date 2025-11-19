fec1df7ee8843ca99361d8913bca41cd7000d242
Heitor Bianchi <72475493+heitorel@users.noreply.github.com>
2025-11-19 16:22:42 -0300

    initial commit

---
+++ src/main/java/com/capao/capitascore/CapitascoreApplication.java
@@ -0,0 +1,13 @@
+package com.capao.capitascore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitascoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitascoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capao/capitascore/CapitascoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capao.capitascore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitascoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
